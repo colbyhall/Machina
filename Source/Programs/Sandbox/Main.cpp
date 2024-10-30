40808499b31ea868afa27d96fb1e9526d24b18e6
Colby Hall <me@cobeh.com>
2024-10-29 20:49:34 -0700

    Added copyright notice to Sandbox/Main.cpp

--- Source/Programs/Sandbox/Main.cpp
+++ Source/Programs/Sandbox/Main.cpp
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2024 Colby Hall <me@cobeh.com>
+ *
+ * This software is released under the MIT License.
+ */
+
 #include <GPU/Device.hpp>
 #include <GUI/Application.hpp>
 #include <GUI/Window.hpp>
