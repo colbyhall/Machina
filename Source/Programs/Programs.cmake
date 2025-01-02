a782e07dde22d6509fd9800e18bcd1cba211e146
Colby Hall <me@cobeh.com>
2025-01-02 14:11:24 -0800

    Removed message from CMakeLists.txt and ensured signing only runs on Apple OS's

--- Source/Programs/Programs.cmake
+++ Source/Programs/Programs.cmake
@@ -3,7 +3,11 @@ function(add_grizzly_executable target root)
     target_include_directories(${target} PRIVATE ${RUNTIME_ROOT} ${THIRD_PARTY_ROOT})
     set_target_properties(${target} PROPERTIES FOLDER "Programs")
     source_group(TREE ${root} FILES ${ARGN})
-	set(TARGETS_TO_SIGN ${TARGETS_TO_SIGN} ${target} PARENT_SCOPE)
+
+	# Append this list of programs to sign if on MacOS
+	if(APPLE)
+		set(TARGETS_TO_SIGN ${TARGETS_TO_SIGN} ${target} PARENT_SCOPE)
+	endif()
 endfunction()
 
 include(${PROGRAMS_ROOT}/Sandbox/Sandbox.cmake)
